e166f6466bd7415e2e4dd86bfe3b768be724b255
Devadharshini R <116997132+devalogi@users.noreply.github.com>
2023-09-21 21:16:54 +0530

    Add files via upload

---
+++ Booking.java
@@ -0,0 +1,41 @@
+package busreserv;
+import java.util.*;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat; 
+public class Booking {
+	String passengerName;
+	int busNo;
+	Date date;
+	
+	Booking(){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter name of passenger: ");
+		passengerName = s.next();
+		System.out.println("Enter bus no: ");
+		busNo = s.nextInt();
+		System.out.println("Enter date dd-mm-yyyy");
+		String dateInput = s.next();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		
+		try {
+			date = dateFormat.parse(dateInput);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean isAvailable() throws SQLException{
+		DAO busdao=new DAO();
+		BookingDAO bookingdao=new BookingDAO();
+		int capacity = busdao.getcapacity(busNo);
+		
+		
+		int booked = bookingdao.getbookedcount(busNo,date);
+		
+		return booked<capacity?true:false;
+		
+	}
+}
